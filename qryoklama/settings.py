1a0481a73b37f1a510a95734d6570e155b7d11a4
dadgosthere <138229016+dadgosthere@users.noreply.github.com>
2024-05-30 01:33:41 +0300

    Update settings.py

--- qryoklama/settings.py
+++ qryoklama/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0aq04hfywrvoq_b8wd*2!d$3o^1u0$&fntitfuypgm+)(dfg+q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
